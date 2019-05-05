bce1183a22dc70171fda7a9ba9d3102482b40d9b
Gabriele Bè <gabriele.be01@gmail.com>
2019-05-05 11:50:15 +0200

    Project default init

---
+++ MDK-ARM/DCU_DP11/port.d
@@ -0,0 +1,12 @@
+dcu_dp11\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM7/r0p1/port.c
+dcu_dp11\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+dcu_dp11\port.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u6\Bin\..\include\stddef.h
+dcu_dp11\port.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u6\Bin\..\include\stdint.h
+dcu_dp11\port.o: ../Inc/FreeRTOSConfig.h
+dcu_dp11\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+dcu_dp11\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+dcu_dp11\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+dcu_dp11\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM7/r0p1/portmacro.h
+dcu_dp11\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+dcu_dp11\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+dcu_dp11\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
---
+++ MDK-ARM/DCU_DP11/startup_stm32f767xx.d
@@ -0,0 +1,1 @@
+dcu_dp11\startup_stm32f767xx.o: startup_stm32f767xx.s
